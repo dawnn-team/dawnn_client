52c5b4c3ee3d8e53f3fe3e35fcf79f15d8a60a36
mhmyesman <41195669+mhmyesman@users.noreply.github.com>
2021-03-03 17:43:32 -0500

    Start working on app navigation.

---
+++ lib/navigation/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:dawn_client/navigation/place_holder.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 1; // Set to 1 straight away to load 'home' page.
+
+  final _barItems = [
+    TabItem(icon: Icons.camera_alt, title: 'Photos'),
+    TabItem(icon: Icons.map, title: 'Map'),
+    TabItem(icon: Icons.settings, title: 'Settings'),
+  ];
+
+  final List<Widget> _children = [PlaceHolder(), PlaceHolder(), PlaceHolder()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: ConvexAppBar(
+        items: _barItems,
+        initialActiveIndex: 1,
+        onTap: onTap,
+      ),
+    );
+  }
+
+  void onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
